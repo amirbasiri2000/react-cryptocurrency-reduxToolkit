0a64246ac24d7af3c2a2a5477fed605686a814d0
amirbasiri2000 <78272810+amirbasiri2000@users.noreply.github.com>
2022-03-27 18:16:34 +0430

    build2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar } from './components';
 import {
-  Exchanges,
   Homepage,
   Cryptocurrencies,
   News,
